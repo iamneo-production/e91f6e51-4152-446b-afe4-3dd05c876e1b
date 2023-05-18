009f6eebdbe0312e3d08e8a29dd76ccf074a711c
Naveenkumar-31 <naveenambala24@gmail.com>
2023-05-18 13:01:47 +0000

    Login&Signup

---
+++ reactapp/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./components/Auth/Login/Login";
+import Signup from "./components/Auth/Signup/Signup";
+import Homepage from "./components/Customer/HomepPage/HomePage"
+import Addtheme from "./components/Admin/Addtheme";
+
+function App() {
+  return (
+    <div className="App">
+  
+      <Routes>
+        {/* PUBLIC ROUTE */}
+        <Route path="/" element={<Login />} />
+        <Route path="/user/login" element={<Login />} />
+        <Route path="/user/signup" element={<Signup />} />
+        <Route path="/admin/login" element={<Login />} />
+
+        {/* USER ROUTES */}
+
+        <Route path="/user/home" element={<Homepage/>} />
+        
+        {/* ADMIN ROUTES */}
+
+        <Route path="/admin/home" element={<Addtheme />} />
+      </Routes>
+
+      
+    </div>
+  );
+}
+
+export default App;
