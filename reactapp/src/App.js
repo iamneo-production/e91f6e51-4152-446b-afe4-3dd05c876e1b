e3e1242f78b24bb57571918e32b7caeff148e61a
Veeraja113 <vtu14015@veltech.edu.in>
2023-05-28 11:31:44 +0000

    BookingPage updated

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,27 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+  BrowserRouter,
+} from "react-router-dom";
 
+
+import theme from "/home/coder/project/workspace/reactapp/src/Theme/Theme.js";
 import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
-import Homepage from "./components/Customer/HomepPage/HomePage"
+import Homepage from "./components/Customer/HomepPage/HomePage";
 import Addtheme from "./components/Admin/Addtheme";
+import Booking from "./components/Customer/BookingPage/Booking";
 
 function App() {
   return (
     <div className="App">
-  
+      <ThemeProvider theme={theme}>
+        
+          
       <Routes>
         {/* PUBLIC ROUTE */}
         <Route path="/" element={<Login />} />
@@ -20,12 +32,15 @@ function App() {
         {/* USER ROUTES */}
 
         <Route path="/user/home" element={<Homepage/>} />
+        <Route path="/user/bookTheme" element={<Booking/>} />
+
         
         {/* ADMIN ROUTES */}
 
         <Route path="/admin/home" element={<Addtheme />} />
       </Routes>
-
+      
+      </ThemeProvider>
       
     </div>
   );
---
+++ reactapp/src/components/Customer/Navbaar/Navbaar.js
@@ -0,0 +1,155 @@
+import AdbIcon from "@mui/icons-material/Adb";
+import CakeIcon from "@mui/icons-material/Cake";
+import LogoutIcon from "@mui/icons-material/Logout";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+const pages = [
+  { id: "UserBookEvent", label: "Book Event", href: "/user/bookTheme" },
+  {
+    id: "UserViewBookEvent",
+    label: "Booked Event",
+    href: "/user/getBookedTheme",
+  },
+];
+
+function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const navigate = useNavigate();
+  let location = useLocation();
+
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = (href) => {
+    navigate(href);
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar color="primary" position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <CakeIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Booking App
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page?.label}
+                  id={page?.id}
+                  onClick={() => handleCloseNavMenu(page?.href)}
+                >
+                  <Typography textAlign="center">{page?.label}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Booking App
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page?.label}
+                id={page?.id}
+                onClick={() => handleCloseNavMenu(page?.href)}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  border:
+                    page?.href === location?.pathname
+                      ? "1px solid #fff"
+                      : "none",
+                }}
+                variant={"text"}
+              >
+                {page?.label}
+              </Button>
+            ))}
+          </Box>
+          <Tooltip arrow title="Logout">
+            <IconButton id="logout" color="error" aria-label="Logout">
+              <LogoutIcon />
+            </IconButton>
+          </Tooltip>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;
