244c448e525772e62656de03c8d262ccf6d931c7
pratiknwd <pratiknwd@gmail.com>
2023-05-29 15:54:43 +0000

    Theme_model_repo created

---
+++ springapp/src/main/java/com/examly/springapp/repository/ThemeModelRepository.java
@@ -0,0 +1,18 @@
+package com.examly.springapp.repository;
+import org.springframework.web.bind.annotation.*;
+import com.examly.springapp.models.*;
+import org.springframework.http.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.*;
+import java.util.*;
+
+@Repository
+
+public interface ThemeModelRepository extends JpaRepository<ThemeModel,Integer> {
+    List<CourseModel> findAll(); 
+    Optional<CourseModel> findBycourseId(Integer id); 
+    void deleteById(Integer id);//used to delete record from table based on id
+    boolean existsByCourseId(Integer id); //method checks if a record exists in table with the specified id
+    
+}
