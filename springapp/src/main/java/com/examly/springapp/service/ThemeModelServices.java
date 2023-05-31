d96a6d3ae06b838c2905361492eed41e858418a3
gannarapupadmapriya <gannarapupadmapriya@gmail.com>
2023-05-31 13:47:50 +0000

    commit

---
+++ springapp/src/main/java/com/examly/springapp/service/ThemeModelServices.java
@@ -0,0 +1,67 @@
+package com.examly.springapp.service;
+import com.examly.springapp.repository.*;
+import com.examly.springapp.models.*;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.*;
+import java.util.*;
+
+@Service
+
+
+public class ThemeModelServices {
+    @Autowired
+    private ThemeModelRepository themeModelRepository;
+
+    public ThemeModelServices(ThemeModelRepository themeModelRepository){
+        this.themeModelRepository = themeModelRepository;
+    }
+
+
+    public void addTheme(ThemeModel themeModel){
+        themeModelRepository.save(themeModel);
+    }
+    // for editing the theme
+    public String editTheme(int themeId, ThemeModel updatedTheme){
+        Optional<ThemeModel> themeid = themeModelRepository.findByThemeId(themeId);
+        if(themeid.isPresent()) {
+
+            ThemeModel theme = themeid.get();
+            theme.setThemeName(updatedTheme.getThemeName());
+            theme.setThemeimgUrl(updatedTheme.getThemeimgUrl());
+            theme.setThemeDescription(updatedTheme.getThemeDescription());
+            theme.setThemephotographer(updatedTheme.getThemephotographer());
+            theme.setThemeVideographer(updatedTheme.getThemeVideographer());
+            theme.setThemeReturnGift(updatedTheme.getThemeReturnGift());
+            theme.setCost(updatedTheme.getCost());
+
+            themeModelRepository.save(theme);
+            return "Themes edited";
+        }
+        else{
+            return "Themes not edited";
+        }
+    }
+
+
+    //for deleting the theme
+    public String deleteTheme(Integer id){
+        Optional<ThemeModel> themeModel=themeModelRepository.findById(id);
+        if(themeModel.isPresent()){
+            themeModelRepository.deleteById(id);
+            return "Student details deleted";
+        }
+        return "Student not Found";
+    }
+
+    public ThemeModel getTheme(Integer id){
+        Optional<ThemeModel> themeModel=themeModelRepository.findById(id);
+        if(themeModel.isPresent()){
+            return themeModel.get();
+        }
+        return null;
+    }
+
+    public List<ThemeModel> getTheme(){
+        return themeModelRepository.findAll();
+    }
+}
