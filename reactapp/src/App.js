c4e3324644aac9062b0caea9b98f01c23a65bcc9
Veeraja113 <vtu14015@veltech.edu.in>
2023-06-02 14:41:28 +0000

    UpdatedChanges

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-  BrowserRouter,
-} from "react-router-dom";
-
 
 import theme from "/home/coder/project/workspace/reactapp/src/Theme/Theme.js";
 import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
-
-//import Homepage from "./components/Customer/HomepPage/HomePage";
-//import Addtheme from "./components/Admin/Addtheme";
+import HomePage from "./components/Customer/HomepPage/HomePage";
 import Booking from "./components/Customer/BookingPage/Booking";
 import ViewBookedEvents from "./components/Customer/ViewBooking/ViewBooking";
-
+import AddTheme from "./components/Admin/AddTheme/AddTheme";
+import AddMenu from "./components/Admin/AddMenu/AddMenu";
+import Addons from "./components/Admin/AddOns/AddOns"
 
 function App() {
   return (
@@ -31,18 +24,22 @@ function App() {
         <Route path="/user/login" element={<Login />} />
         <Route path="/user/signup" element={<Signup />} />
         <Route path="/admin/login" element={<Login />} />
+        
 
         {/* USER ROUTES */}
-        <Route path="/user/bookTheme" element={<Booking/>} />
-       {/* <Route path="/user/home" element={<Homepage/>} />
-        */}
-        <Route path="/user/getBookedTheme" element={<ViewBookedEvents/>} />
-  
-        
-        {/* ADMIN ROUTES 
 
-  <Route path="/admin/home" element={<AddTheme />} />*/}
-  </Routes>
+        <Route path="/user/getAllThemes" element={<HomePage/>} />
+        <Route path="/user/bookTheme" element={<Booking/>} />
+        <Route path="/user/bookTheme/:id" element={<Booking/>} />
+        <Route path="/user/getBookedTheme" element={<ViewBookedEvents />} />
+
+        
+        {/* ADMIN ROUTES */}
+
+        <Route path="/admin/home" element={<AddTheme />} />
+        <Route path="/admin/addMenu" element={<AddMenu/>} />
+        <Route path="/admin/addAddon" element={<Addons/>}/>
+        </Routes>
       
       </ThemeProvider>
       
