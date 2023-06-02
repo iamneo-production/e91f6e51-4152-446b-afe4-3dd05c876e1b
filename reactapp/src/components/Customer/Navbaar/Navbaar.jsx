1d8dedcaabc3393ffbda397a338051bf07261d60
Veeraja113 <vtu14015@veltech.edu.in>
2023-06-02 14:41:28 +0000

    UpdatedChanges

--- reactapp/src/components/Customer/Navbaar/Navbaar.jsx
+++ reactapp/src/components/Customer/Navbaar/Navbaar.jsx
@@ -1,1 +1,29 @@
-/home/coder/project/workspace/node_modules
+import React from 'react';
+import styles from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar_div}>
+      <div className={styles.logo}>
+        <p>Birthday Event</p>
+      </div>
+      <div className={styles.page_section}>
+        <NavLink to="/admin/home" className={styles.links} activeClassName={styles.activeLink}>
+          Add Theme
+        </NavLink>
+        <NavLink to="/admin/addMenu" className={styles.links} activeClassName={styles.activeLink}>
+          Add Menu
+        </NavLink>
+        <NavLink to="/admin/addAddon" className={styles.links} activeClassName={styles.activeLink}>
+          Add Add-ons
+        </NavLink>
+      </div>
+      <div className={styles.logout}>
+        <p>Logout</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
