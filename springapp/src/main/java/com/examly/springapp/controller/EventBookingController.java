9ee2bbf6bd92d6f30186bddf85707ed045bc2ed1
Veeraja113 <vtu14015@veltech.edu.in>
2023-06-15 06:29:22 +0000

    Booking

--- springapp/src/main/java/com/examly/springapp/controller/EventBookingController.java
+++ springapp/src/main/java/com/examly/springapp/controller/EventBookingController.java
@@ -1,4 +1,38 @@
+package com.examly.springapp.controller;
 
+import org.springframework.web.bind.annotation.*;
+import com.examly.springapp.service.*;
+import com.examly.springapp.models.*;
+
+import org.springframework.beans.factory.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EventBookingController {
-    
+    @Autowired
+    private EventModelServices eventService;
+
+    @PostMapping("user/addEvent")
+    public String saveNewEvent(@RequestBody EventModel eventModel) {
+        eventService.addEvent(eventModel);
+        return "Event added";
+    }
+
+    @DeleteMapping("user/deleteEvent/{id}")
+    private String deleteEvent(@PathVariable("id") Integer eventId) {
+        return eventService.deleteEvent(eventId);
+    }
+
+    @PutMapping("user/editEvent/{id}")
+    private String editEvent(@PathVariable("id") Integer eventId, @RequestBody EventModel updatedEvent) {
+        return eventService.editEvent(eventId, updatedEvent);
+    }
+
+    @GetMapping("user/viewEvent")
+    public List<EventModel> viewEvents() {
+        return eventService.getEventList();
+    }
 }
