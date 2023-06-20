4dea59aa9107a76708ec439b5c8fa1d029fc7bb6
pratiknwd <pratiknwd@gmail.com>
2023-06-20 17:22:47 +0000

    booking page restore

--- reactapp/src/components/Customer/BookingPage/Booking.jsx
+++ reactapp/src/components/Customer/BookingPage/Booking.jsx
@@ -79,8 +79,9 @@ export default function Booking() {
      <div className="apply-form">
       {currentPage === 1 && (
         <form className="form-container">
-       <div className="floating-label-group">
-       <input
+       
+          
+            <input
               data-testid="enterName"
               id="enterName"
               className="input-form"
@@ -91,8 +92,7 @@ export default function Booking() {
               readOnly
               style={{ fontWeight: 'bold'}}
               onChange={handleChange}
-            /><label class="floating-label">Enter Event  Name</label>
-            </div>
+            />
          
           
               <input
@@ -188,7 +188,7 @@ export default function Booking() {
               data-testid="enterMonths"
               onChange={handleChange}
             />
-     
+         
         </form>
       )}
       {currentPage === 2 && <BookEventSecondPage handleSubmit={handleSubmit} eventData={eventData} setEventData={setEventData}/>}
@@ -219,6 +219,5 @@ export default function Booking() {
     </div>
   );
 }
-
 
 
