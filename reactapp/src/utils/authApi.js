1b725af53678bb46a6eaa56f23612b0799c7939e
pratiknwd <pratiknwd@gmail.com>
2023-07-12 16:17:57 +0000

    login changes

--- reactapp/src/utils/authApi.js
+++ reactapp/src/utils/authApi.js
@@ -1,7 +1,7 @@
 
 //Define base url for all operations
 
-export const BaseUrl = "https://8080-faeeaababbcfcafefbdecbeaedcfdfabbdb.project.examly.io";
+export const BaseUrl = "https://8080-ceddbecfbbacaefefbdecbedaeddda.project.examly.io";
 
 
 
