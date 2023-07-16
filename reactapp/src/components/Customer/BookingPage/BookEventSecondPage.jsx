822a2cc4284d6d852896bbe0ca70eb383ccd8367
ajaysairam482001 <ajaysairamn.ece.sec@gmail.com>
2023-07-16 06:24:45 +0000

    push branch

--- reactapp/src/components/Customer/BookingPage/BookEventSecondPage.jsx
+++ reactapp/src/components/Customer/BookingPage/BookEventSecondPage.jsx
@@ -66,11 +66,7 @@ export default function BookEventSecondPage({ eventData, setEventData }) {
       const res = await axios.post(`${BaseUrl}/user/addEvent`, eD, { headers });
       Swal.fire("Success", res.data, "success");
       console.log(res.data);
-<<<<<<< HEAD
-      Swal.fire("Success", res.data, "success");
-=======
       // alert(res.data);
->>>>>>> 7b3575c07c9cc4e062ac3cb2b2909a30395d9cf0
     } catch (e) {}
   }
 
