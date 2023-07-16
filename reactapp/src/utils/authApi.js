c8d9d403fe50b25d36d9f874fb60dbce07765e6f
Hemnath0075 <hemnath5700@gmail.com>
2023-07-16 09:57:25 +0000

    event details css

--- reactapp/src/utils/authApi.js
+++ reactapp/src/utils/authApi.js
@@ -1,6 +1,6 @@
 
 //Define base url for all operations
-export const BaseUrl = "https://8080-ceddbecfbbacaefefbdecbedaeddda.project.examly.io";
+export const BaseUrl = "https://8080-fbfbcfdafefbdecbeaedcfdfabbdb.project.examly.io";
  
 
 export async function signUpUser(email, mobileNumber, password, userType, userName) {
