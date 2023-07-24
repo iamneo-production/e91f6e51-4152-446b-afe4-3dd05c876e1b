543a5c8d139f7e9ef7487ae2d413a6c2a5e21e9c
pratiknwd <pratiknwd@gmail.com>
2023-07-24 22:35:59 +0000

    css changes

--- reactapp/src/components/Admin/Navbar/Navbar.js
+++ reactapp/src/components/Admin/Navbar/Navbar.js
@@ -1,44 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
-import { NavLink,useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 
 const Navbar = () => {
   const location = useLocation();
- function handleLogout(){
-  localStorage.setItem("userModel",null);
+  function handleLogout() {
+    localStorage.setItem("userModel", null);
   }
-  const [active,setActive]=useState(0);
-  useEffect(()=>{
+  const [active, setActive] = useState(0);
+  useEffect(() => {
     console.log(location.pathname);
-    if(location.pathname=="/admin/home")
+    if (location.pathname == "/admin/home")
       setActive(0);
-    else if (location.pathname=="/admin/addMenu")
+    else if (location.pathname == "/admin/addMenu")
       setActive(1);
-    else if (location.pathname=="/admin/addAddon")
+    else if (location.pathname == "/admin/addAddon")
       setActive(2);
-  },[location.pathname])
+  }, [location.pathname])
   return (
     <div className={styles.navbar_div}>
       <div className={styles.logo}>
         <p>Birthday Event</p>
       </div>
       <div className={styles.page_section}>
-        <NavLink to="/admin/home" className={active==0?styles.active_link:styles.links} activeClassName={styles.activeLink}>
+        <NavLink to="/admin/home" className={active == 0 ? styles.active_link : styles.links} activeClassName={styles.activeLink}>
           Add Theme
         </NavLink>
-        <NavLink to="/admin/addMenu" className={active==1?styles.active_link:styles.links} activeClassName={styles.activeLink}>
+        <NavLink to="/admin/addMenu" className={active == 1 ? styles.active_link : styles.links} activeClassName={styles.activeLink}>
           Add Menu
         </NavLink>
-        <NavLink to="/admin/addAddon" className={active==2?styles.active_link:styles.links} activeClassName={styles.activeLink}>
+        <NavLink to="/admin/addAddon" className={active == 2 ? styles.active_link : styles.links} activeClassName={styles.activeLink}>
           Add Add-ons
         </NavLink>
       </div>
-      <NavLink to="/user/login" className={styles.logout} activeClassName={styles.logout} onClick={handleLogout} >
+      <NavLink to="/user/login" className={styles.logout} activeClassName={styles.logout} onClick={handleLogout}>
+        <i className="fa-solid fa-right-from-bracket"></i>
         Logout
-        </NavLink>
-      </div>
-    
+      </NavLink>
+    </div>
+
   );
 };
 
