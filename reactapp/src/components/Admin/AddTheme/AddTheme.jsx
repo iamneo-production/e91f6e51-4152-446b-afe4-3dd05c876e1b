a5e846c6fefc3c3bd640f4cbc1247f4298938eda
Goblin <99012020+pratiknwd@users.noreply.github.com>
2023-07-24 11:42:09 +0530

    Revert "Birthday event booking system  veeraja113"

--- reactapp/src/components/Admin/AddTheme/AddTheme.jsx
+++ reactapp/src/components/Admin/AddTheme/AddTheme.jsx
@@ -1,21 +1,19 @@
 import React, { useRef, useState, useContext, useEffect } from "react";
-// import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import Navbar from "../Navbar/Navbar";
 import styles from "./AddTheme.module.css";
 import { BaseUrl } from "../../../utils/authApi";
 import axios from "axios";
 import UserContext from "../../../UserContext";
+import { useNavigate } from "react-router-dom";
 
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import TextField from "@mui/material/TextField";
 import Swal from "sweetalert2";
-import { DeleteOutlined,EditOutlined  } from "@ant-design/icons";
-
-
 const { Meta } = Card;
 
 const AddTheme = () => {
-  const { appUser, setAppUser } = useContext(UserContext);
+  const { appUser, setAppUserl } = useContext(UserContext);
   const themeName = useRef();
   const imageUrl = useRef();
   const photographerDetails = useRef();
@@ -23,7 +21,7 @@ const AddTheme = () => {
   const returnGift = useRef();
   const themeCost = useRef();
   const description = useRef();
-  const location = useRef();
+  const navigate = useNavigate();
   const [data, setData] = useState([]);
 
   const jwtToken = appUser?.token;
@@ -32,8 +30,9 @@ const AddTheme = () => {
     Authorization: `Bearer ${jwtToken}`,
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  async function handleSubmit() {
+    console.log("i am handle submit");
+  
     // Check if any required field is empty
     const requiredFields = [
       themeName.current.value,
@@ -43,11 +42,10 @@ const AddTheme = () => {
       returnGift.current.value,
       themeCost.current.value,
       description.current.value,
-      location.current.value,
     ];
-
+  
     const emptyFields = requiredFields.filter((field) => !field || field.trim() === "");
-
+  
     if (emptyFields.length > 0) {
       const message = `Please fill all the fields before proceeding further`;
       Swal.fire("Error", message, "error");
@@ -59,7 +57,6 @@ const AddTheme = () => {
       });
       return;
     }
-
     const themeModel = {
       themeName: themeName.current.value,
       themeimgUrl: imageUrl.current.value,
@@ -68,17 +65,17 @@ const AddTheme = () => {
       themeReturnGift: returnGift.current.value,
       cost: themeCost.current.value,
       themeDescription: description.current.value,
-      location: location.current.value,
     };
 
-
+    console.log("data sent to backend",themeModel)
 
     try {
       const res = await axios.post(`${BaseUrl}/admin/addTheme`, themeModel, { headers });
       console.log("return from backend", res.data);
-      Swal.fire("Success", "Theme added successfully!", "success");
+      Swal.fire("Success", res.data, "success");
       // Refresh the data after adding the theme
       getAllThemes();
+  
       // Reset the form fields
       themeName.current.value = "";
       imageUrl.current.value = "";
@@ -87,18 +84,20 @@ const AddTheme = () => {
       returnGift.current.value = "";
       themeCost.current.value = "";
       description.current.value = "";
-      location.current.value = "";
     } catch (e) {
       console.log(e.message);
       Swal.fire("Error", "An error occurred while submitting the form.", "error");
     }
   };
 
+     
+  
+
   const getAllThemes = async () => {
     try {
       const res = await axios.get(`${BaseUrl}/admin/theme`, { headers });
-      console.log("res", res.data);
-
+      console.log("res",res.data);
+      
       // Reverse the data array to display the last entry first
       const reversedData = res.data.reverse();
 
@@ -108,23 +107,27 @@ const AddTheme = () => {
     }
   };
 
-  async function handleDelete(row) {
-    console.log(row);
-    try {
-      const res = await axios.delete(`${BaseUrl}/admin/deleteTheme/${row.themeId}`, { headers });
-      const data = res.data;
-      console.log("res after delete", data);
-
-      setData((prevItems) => prevItems.filter((item) => item.themeId !== row.themeId));
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  // Fetch the data initially
+  //to fetch the data in starting  without it it will not fetch data
   useEffect(() => {
     getAllThemes();
   }, []);
+
+    async function handleDelete(row) {
+    console.log(row);
+    try {
+      const res = await axios.delete(
+        `${BaseUrl}/admin/deleteTheme/${row.themeId}`, { headers }
+      );
+      const data = res.data;
+      console.log("res after delte", data);
+
+      setData((prevItems) =>
+        prevItems.filter((item) => item.themeId !== row.themeId)
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   return (
     <>
@@ -132,90 +135,87 @@ const AddTheme = () => {
       <div className={styles.main_content}>
         <div className={styles.container}>
           <div className={styles.theme_form}>
-            <form onSubmit={handleSubmit}>
-              <div className={styles.form_row}>
-                <TextField
-                  type="text"
-                  label="Enter Theme Name"
-                  fullWidth
-                  inputRef={themeName}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Image URL"
-                  fullWidth
-                  inputRef={imageUrl}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Photographer Details"
-                  fullWidth
-                  inputRef={photographerDetails}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Videographer Details"
-                  fullWidth
-                  inputRef={videographerDetails}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Theme Return Gift"
-                  fullWidth
-                  inputRef={returnGift}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Theme Cost"
-                  fullWidth
-                  inputRef={themeCost}
-                />
-                <TextField
-                  type="text"
-                  label="Enter Theme Description"
-                  fullWidth
-                  multiline
-                  inputRef={description}
-                />
-                <TextField
-                  type="text"
-                  label="Location"
-                  fullWidth
-                  inputRef={location}
-                />
-              </div>
+            <div className={styles.form}>
+            <TextField
+                type="text"
+                label="Enter Theme Name"
+                ref={themeName}
+              />
+              <TextField type="text" label="Enter Image URL" ref={imageUrl} />
+              <TextField
+                type="text"
+                label="Enter Photographer Details"
+                ref={photographerDetails}
+              />
+               <TextField
+                type="text"
+                label="Location"
+                
+              />
               <div className={styles.add_btn_container}>
-                <button type="submit" className={styles.add_btn}>
-                  Add Theme
-                </button>
+                <TextField
+                  type="submit"
+                  value="Add Theme"
+                  className={styles.add_btn}
+                  onClick={handleSubmit}
+                />
               </div>
-            </form>
+            </div>
+            <div className={styles.form}>
+              <TextField
+                type="text"
+                label="Enter Videographer Details"
+                ref={videographerDetails}
+              />
+              <TextField
+                type="text"
+                label="Enter Theme Return Gift"
+                ref={returnGift}
+              />
+              <TextField
+                type="text"
+                label="Enter Theme Cost"
+                ref={themeCost}
+              />
+              
+              <TextField
+                type="text"
+                label="Enter Theme Description"
+                cols={20}
+                rows={10}
+                ref={description}
+              />
+            </div>
           </div>
           <div className={styles.themes_view}>
-            <div className={styles.theme_cards_wrapper}>
-              {data?.map((item) => (
-                <div>
-                  <Card className={styles.card} hoverable>
-                    <img alt="example" src={item.themeimgUrl} />
-                    <div className={styles.event_details}>
-                      <div className={styles.event_details_place}>
-                        <span className={styles.event_text}>{item.themeName}</span>
-                        <span className={styles.event_text}>₹ {item.cost}</span>
-                      </div>
-                    </div>
-                    <EditOutlined
-                          className={styles.edit_icon}
-                          // onClick={() => handleEdit(item)} // Add your edit functionality here
-                        />
-                    <div className={styles.card_actions}>
-                      <DeleteOutlined
-                        className={styles.delete_icon}
-                        onClick={() => handleDelete(item)}
+            <div className={styles.card_scroll_view}>
+              {data?.map((item)=>{
+              return(
+                <Card
+                key={item.themeId}
+                    hoverable
+                    style={{
+                      width: 300,
+                    }}
+                    cover={
+                      <img
+                        alt="example"
+                        src={item.themeimgUrl}
                       />
-                    </div>
+                    }
+                    actions={[
+                      <EditOutlined key="edit" onClick={() => {
+                        console.log("clicked")
+                      }}/>,
+                      <DeleteOutlined key="ellipsis" onClick={() => handleDelete(item)}/>,
+                    ]}
+                  >
+                    <Meta
+                      title={item.themeName}
+                      description={`₹ ${item.cost}`}
+                    />
                   </Card>
-                </div>
-              ))}
+              )})}
             </div>
           </div>
         </div>
