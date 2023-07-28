9e06d2c3388b57ab85960fed6b6d082e8ad69817
pratiknwd <pratiknwd@gmail.com>
2023-07-28 20:24:56 +0000

    security update

--- springapp/src/main/java/com/examly/springapp/security/securityconfig/SecurityConfig.java
+++ springapp/src/main/java/com/examly/springapp/security/securityconfig/SecurityConfig.java
@@ -60,7 +60,8 @@ private UserDetailsServiceImpl userDetailsServiceImpl;
         http.cors().and().csrf().disable()
         .exceptionHandling().authenticationEntryPoint(authEntryPoint).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        //.authorizeRequests().antMatchers("/user/login/**","/user/signup/**","/admin/signup/**","/admin/login/**","/").permitAll()
+        // .authorizeRequests().antMatchers("/user/login/**","/user/signup/**","/admin/signup/**","/admin/login/**","/",
+        // "/admin/event/**","/admin/theme/**","/admin/add-on/**","/admin/menu/**").permitAll()
         .authorizeRequests().antMatchers("/**").permitAll()
         .anyRequest().authenticated();
         http.authenticationProvider(authenticationProvider());
